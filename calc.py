00708284953f44b39b210ae1fb2a98d20caf68b0
P-etetr-shi <pwanjala404@gmail.com>
2025-02-25 15:16:07 +0300

    push all the files

---
+++ calc.py
@@ -0,0 +1,25 @@
+#creating a first simple calculator
+#initialiaze the data type and user input
+"""
+num1=float(input("Enter first number: "))
+#allow user input the second number
+num2=float(input("Enter second number: "))
+
+#now the simple calculatortimesResul
+timesResult=num1*num2
+diffeResult=num1-num2
+divisionResult=num1/num2
+addResult=num1+num2
+#now lets perfom the calculatioins
+print(f"Sum: {timesResult}")
+print(f"Diffrence: {diffeResult}")
+print(f"Division: {divisionResult}")
+print(f"Addition: {addResult}")
+
+"""
+from numpy import random
+test=int(random.rand()*100)
+print(test)
+
+x=random.choice([3,0,9,5,4],p=[0.3,0.1,0.0,0.2,0.4],size=(2,5))
+print(x)
